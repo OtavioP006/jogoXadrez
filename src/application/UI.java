2ac1d925b032d8fbc5d11be63d519b7e73b9329e
unknown <otaviopassarelli5@gmail.com>
2021-03-23 11:23:50 -0300

    Colocando peças no tabuleiro

--- src/application/UI.java
+++ src/application/UI.java
@@ -12,12 +12,12 @@ public class UI {
 			System.out.println();
 		}
 		
-		System.out.print("    A   B   C   D   E    F   G   H");
+		System.out.print("  A  B  C  D  E  F  G  H");
 	}
 	
 	private static void printPiece(ChessPiece piece) {
 		if (piece == null) {
-			System.out.print("  - ");
+			System.out.print(" - ");
 		}
 		else {
 			System.out.print(piece);
